380d9010f2afa64c9c26b16c59fe85466983344d
Tfactor08 <Tfactor2020@gmail.com>
2025-04-27 21:12:40 +0300

    start handling nested objects

--- ast_to_py.py
+++ ast_to_py.py
@@ -1,7 +1,7 @@
 """Generate Python objects from AST"""
 
 # TODO
-# handle different types (currently everything is converted to str) 
+# handle different types (currently everything is converted to str);
 # unhardcode __str__() generation (should loop over object's attributes);
 # handle nested objects;
 # how to name objects without a label?
@@ -9,18 +9,23 @@
 from _ast_ import *
 from parser import produce_ast
 
-json = '{"name": "foo", "age": 69}'
+json = '{"name": "foo", "age": 69, "some_obj": {"a": 69}}'
 
 def gen_obj(obj: Object):
     assert isinstance(obj, Object), f"{obj} is not of type Object"
     attrs = {attr.label: attr.value for attr in obj.body}
     py_attrs = dict()
     for label, value in attrs.items():
-        assert isinstance(value, Literal), "We don't handle non-literal values yet"
-        py_attrs[label.strip('"')] = value.value
-    print(py_attrs)
-    # Unhardcode
-    py_attrs['__str__'] = lambda self: f"name: {self.name}, age: {self.age}"
+        #match type(value):
+        #    case Literal:
+        #        print(value)
+        if isinstance(value, Object):
+            py_attrs[label.strip('"')] = gen_obj(value)
+        else:
+            assert isinstance(value, Literal), "We only handle Object and Literal types for now"
+            py_attrs[label.strip('"')] = value.value
+    # TODO Unhardcode
+    py_attrs['__str__'] = lambda self: f"name: {self.name}, age: {self.age}, some_obj: {self.some_obj}"
     SomeObject = type('SomeObject', (object,), py_attrs)
     return SomeObject()
 
